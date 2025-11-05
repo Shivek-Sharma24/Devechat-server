d0091756e632d4d0eea9241750e1bd4eddbe2185
Shivek-Sharma24 <shiveksharma24@gmail.com>
2025-11-05 15:17:57 +0530

    Second commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 5000
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://devchat-com.vercel.app',
     credentials:true
 }))
 
